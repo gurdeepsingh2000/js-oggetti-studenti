1f0469ab4fcb1123d9e8c4cecf323c5e637aa9d2
gurdeepsingh2000 <gurdeepsingh20000329@gmail.com>
2021-06-15 15:44:54 +0200

    udpate

--- js/my-script.js
+++ js/my-script.js
@@ -1,4 +1,6 @@
 var output = document.getElementById('text')
+var output2 = document.getElementById('text2')
+
 
 /*PARTE 1
 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. Stampare a 
@@ -15,14 +17,39 @@ for (var k in studente){    // CON VAR k RICHIEDO LA KEY DELL'OBJECT CHE SI TROV
     output.innerHTML += k + ' ' + studente[k] + '<br>'   //STAMPO la key dell'object (esempio Name) + il valore della key
 }
 
-
-
-
 /*
 PARTE 2
 creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, 
 nome e cognome.  Ricordatevi, è un array, quindi? for.
 */
+
+var ArrayStudenti = [{
+    Name : 'Elon',   
+    Surname: 'Musk',
+    Age: 20
+},
+{
+    Name : 'Snopp',   
+    Surname: 'Dogg',
+    Age: 16
+},
+
+{
+    Name : 'Bernie',   
+    Surname: 'Sanders',
+    Age: 19
+}
+]
+
+for(var x = 0; ArrayStudenti.length > x; x++ ){
+    console.log(ArrayStudenti[x])
+}
+
+for(var k in ArrayStudenti[x]){
+    console.log(k, ArrayStudenti[x][k])
+    output2.innerHTML += k + ' ' + ArrayStudenti[x][k]
+}
+
 
 /*
 PARTE 3
